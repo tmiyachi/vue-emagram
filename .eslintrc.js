3ad078367dee88d3e50d23d712da9bf142afb4ee
tmiyachi <t.miyachi@hotmail.co.jp>
2022-06-28 20:54:59 +0900

    ライブラリ更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,8 @@ module.exports = {
     node: true,
   },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
   rules: {
-    'vue/html-self-closing': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
+    'consistent-return': 'error',
+    'no-sequences': 'error',
   },
 };
